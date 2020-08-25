b1b8ea61e5f70359b2ef209b58c2417266632709
rabobahago <rabobahago@gmail.com>
2020-08-25 17:33:05 +0100

    first post middleware added

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const express = require('express')
 
 const app = express()
+
 app.use((req, res, next) => {
-  console.log('Require received!')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  )
   next()
 })
-app.use((req, res, next) => {
-  res.json({ message: 'successfully created using express' })
-  next()
-})
-app.use((req, res) => {
-  console.log('Header sent successully')
+
+app.use('/api/stuff', (req, res) => {
+  const stuff = [
+    {
+      _id: 'regdggh',
+      title: 'my first stuff for sale',
+      description: 'A clean first stuff for sale with discount included',
+      imageUrl: '',
+      price: 4000,
+      userId: '',
+    },
+    {
+      _id: 'tebfh',
+      title: 'my second stuff for sale',
+      description: 'A clean second stuff for sale with discount included',
+      imageUrl: '',
+      price: 7000,
+      userId: '',
+    },
+  ]
+  res.status(201).json(stuff)
 })
 module.exports = app
