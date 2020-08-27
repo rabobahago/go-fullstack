e3769a400f78ee6676621fdc4a09f0bdf47a0eac
rabobahago <rabobahago@gmail.com>
2020-08-27 09:13:09 +0100

    delete route was added

--- app.js
+++ app.js
@@ -90,6 +90,21 @@ app.put('/api/stuff/:id', (req, res, next) => {
       })
     })
 })
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: 'deleted!',
+      })
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      })
+    })
+})
+
 app.use('/api/stuff', (req, res, next) => {
   Thing.find()
     .then((things) => {
