f2eea665b01c743d0a4632104d3bafe0f0b56bf9
rabobahago <rabobahago@gmail.com>
2020-08-27 08:46:12 +0100

    update route added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Thing = require('./models/thing')
 const app = express()
 mongoose
   .connect(
-    'mongodb+srv://raboski:y8HmOnYfokPSaFnc@cluster0.dmr0d.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    'mongodb+srv://raboski:lWxWJKXiHlTCqrT3@cluster0.dmr0d.mongodb.net/<dbname>?retryWrites=true&w=majority',
   )
   .then(() => {
     console.log('successfully connected to mongoDB atlas')
@@ -64,6 +64,32 @@ app.get('/api/stuff/:id', (req, res, next) => {
       })
     })
 })
+app.put('/api/stuff/:id', (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId,
+  })
+  Thing.updateOne(
+    {
+      _id: req.params.id,
+    },
+    thing,
+  )
+    .then(() => {
+      res.status(201).json({
+        message: 'successfully updated thing in our database',
+      })
+    })
+    .catch((err) => {
+      res.status(404).json({
+        error: error,
+      })
+    })
+})
 app.use('/api/stuff', (req, res, next) => {
   Thing.find()
     .then((things) => {
@@ -77,6 +103,6 @@ app.use('/api/stuff', (req, res, next) => {
 })
 module.exports = app
 
-//y8HmOnYfokPSaFnc
-//mongodb+srv://raboski:<password>@cluster0.dmr0d.mongodb.net/<dbname>?retryWrites=true&w=majority
+//lWxWJKXiHlTCqrT3
+//mongodb+srv://raboski:lWxWJKXiHlTCqrT3@cluster0.dmr0d.mongodb.net/<dbname>?retryWrites=true&w=majority
 //https://upload.wikimedia.org/wikipedia/commons/e/e3/Canon_EOS_60D_01.jpg
