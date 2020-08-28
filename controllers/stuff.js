10f91d084911df345a5a4ba8f780f8d0365d5ab7
rabobahago <rabobahago@gmail.com>
2020-08-28 16:10:52 +0100

    end of project

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require('../models/thing')
+const fs = require('fs')
 
 exports.createThings = (req, res, next) => {
   const url = req.protocol + '://' + req.get('host')
@@ -39,37 +40,32 @@ exports.getOneThing = (req, res, next) => {
 }
 
 exports.updateOneThing = (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId,
-  })
-  Thing.updateOne(
-    {
+  let thing = new Thing({ _id: req.params._id })
+  if (req.file) {
+    const url = req.protocol + '://' + req.get('host')
+    req.body.thing = JSON.parse(req.body.thing)
+    thing = {
       _id: req.params.id,
-    },
-    thing,
-  )
+      title: req.body.thing.title,
+      description: req.body.thing.description,
+      imageUrl: url + '/images/' + req.file.filename,
+      price: req.body.thing.price,
+      userId: req.body.thing.userId,
+    }
+  } else {
+    thing = {
+      _id: req.params.id,
+      title: req.body.title,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl,
+      price: req.body.price,
+      userId: req.body.userId,
+    }
+  }
+  Thing.updateOne({ _id: req.params.id }, thing)
     .then(() => {
       res.status(201).json({
-        message: 'successfully updated thing in our database',
-      })
-    })
-    .catch((err) => {
-      res.status(404).json({
-        error: error,
-      })
-    })
-}
-
-exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => {
-      res.status(200).json({
-        message: 'deleted!',
+        message: 'Thing updated successfully!',
       })
     })
     .catch((error) => {
@@ -77,6 +73,25 @@ exports.deleteThing = (req, res, next) => {
         error: error,
       })
     })
+}
+
+exports.deleteThing = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id }).then((thing) => {
+    const filename = thing.imageUrl.split('/images/')[1]
+    fs.unlink('images/' + filename, () => {
+      Thing.deleteOne({ _id: req.params.id })
+        .then(() => {
+          res.status(200).json({
+            message: 'Deleted!',
+          })
+        })
+        .catch((error) => {
+          res.status(400).json({
+            error: error,
+          })
+        })
+    })
+  })
 }
 
 exports.getAllThings = (req, res, next) => {
--- routes/stuff.js
+++ routes/stuff.js
@@ -7,7 +7,7 @@ const multer = require('../middleware/multer-config')
 router.get('/', auth, stuffCtrl.getAllThings)
 router.post('/', auth, multer, stuffCtrl.createThings)
 router.get('/:id', auth, stuffCtrl.getOneThing)
-router.put('/:id', auth, stuffCtrl.updateOneThing)
+router.put('/:id', auth, multer, stuffCtrl.updateOneThing)
 router.delete('/:id', auth, stuffCtrl.deleteThing)
 
 module.exports = router
