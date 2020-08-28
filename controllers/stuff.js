c81fa60a415cde318f8f6efc33577d2a88f4b92a
rabobahago <rabobahago@gmail.com>
2020-08-28 15:39:25 +0100

    images files multer config added

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,12 +1,14 @@
 const Thing = require('../models/thing')
 
 exports.createThings = (req, res, next) => {
+  const url = req.protocol + '://' + req.get('host')
+  req.body.thing = JSON.parse(req.body.thing)
   const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId,
+    title: req.body.thing.title,
+    description: req.body.thing.description,
+    imageUrl: url + '/images/' + req.file.filename,
+    price: req.body.thing.price,
+    userId: req.body.thing.userId,
   })
   thing
     .save()
